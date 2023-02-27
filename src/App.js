fa599db0311bfeb457a12a673eac327f875bb627
rahul <rsrsrahul959@gmail.com>
2023-02-28 01:02:16 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import axios from "axios";
 
 function App() {
+  const [url, setUrl] = useState();
+  const [shortUrl, setShortUrl] = useState();
+  const handleCLick = () => {
+    axios
+      .post("http://localhost:4000/url", {
+        url
+      })
+      .then((res) => {
+        setShortUrl("http://localhost:4000/" + res.data.id);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column" justifyContent="center" alignItems="center">
+      <Grid item>
+        <TextField
+          id="outlined-basic"
+          label="URL"
+          placeholder="Enter any URL"
+          variant="outlined"
+          margin="dense"
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" onClick={handleCLick}>
+          Shorten
+        </Button>
+      </Grid>
+      <Grid container direction="row" xs={4}>
+        <Grid item xs={11}>
+          <TextField
+            disabled
+            id="outlined-basic"
+            label="short Url"
+            defaultValue={shortUrl ?? "shortened url"}
+            variant="outlined"
+            margin="dense"
+            fullWidth></TextField>
+        </Grid>
+        <Grid item xs={1}>
+          <Button
+            style={{ height: 40, marginTop: 15 }}
+            onClick={() => {
+              navigator.clipboard.writeText(shortUrl);
+            }}>
+            <ContentCopyIcon />
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
